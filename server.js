8cb9e5e4481f0b5e6e8cff7ad50e05ba4ac6b634
Bhimavarapu-Bhavana <bhimavarapubhavana513@gmail.com>
2026-06-02 16:15:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const configPath = path.join(__dirname, 'data', 'defaultConfig.json');
 app.use(cors());
 app.use(express.json());
 app.use(express.static(publicDir));
-
+app.post('/api/deploy', (req, res) => {
+  console.log('Deployment triggered:', req.body);
+  res.status(200).json({ message: 'Deployment initiated' });
+});
 let currentConfig = loadConfig();
 
 function loadConfig() {
